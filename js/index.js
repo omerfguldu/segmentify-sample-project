6ff7751a14ebc4ae684855888c286d9ac8536b39
gulduomer <43722346+omerfguldu@users.noreply.github.com>
2022-10-11 18:32:32 +0300

    urun listesinin json dosyasindan dinamik olarak goruntulemesi yapildi. secilen kategoriye ait urunlerin listelenmesi islemi tamamlandi.

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ const productObj = { ...productData.responses[0] };
 const userCategories = productObj[0].params.userCategories;
 const recommendedProducts = productObj[0].params.recommendedProducts;
 let selectedCategory = "Size Özel";
+const productListContainer = document.querySelector(".product-list-container");
 
 let editedUserCategories = userCategories.map((category) => {
   const categoryArr = category.split(">");
@@ -50,5 +51,26 @@ function onCategory(e, i) {
 }
 
 function showSelectedCategory(categoryProducts) {
-  console.log(categoryProducts);
+  productListContainer.innerHTML = ``;
+  const { add } = splide.Components.Slides;
+  categoryProducts.forEach((category, index) => {
+    const divEl = document.createElement("div");
+    divEl.classList.add("splide__slide");
+    divEl.classList.add("product-container");
+    divEl.setAttribute("id", `splide01-slide0${index + 1}`);
+    divEl.innerHTML = `
+      <img src="${category.image}" alt="aneirin" />
+      <p class="product-title">${category.name}</p>
+      <div class="price-container">
+        <p class="price-text">${category.priceText}</p>
+      </div>
+      <div class="cargo-container">
+        <i class="fa-solid fa-truck"></i>
+        <p>${category.params.shippingFee}</p>
+      </div>
+    `;
+    add(divEl);
+
+    console.log(category.image);
+  });
 }
