2d564f4e92aee69724279a39d4cf691adc89b70e
gulduomer <43722346+omerfguldu@users.noreply.github.com>
2022-10-13 09:27:03 +0300

    proje mobil responsive hale getirildi. product cardlarda yer alan resimlere lazy loading ozelligi eklendi.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 import productData from "../data/product-list.json" assert { type: "json" };
 
+// SplideJS kutuphanesini kullanarak olusturulan slider icin gerekli ayarlamalarin yapilmasi ve mount edilmesi
 const splide = new Splide(".splide", {
   type: "slide",
   gap: "20px",
@@ -8,63 +9,44 @@ const splide = new Splide(".splide", {
   perMove: 1,
   arrows: true,
   pagination: false,
+  lazyLoad: "nearby",
 });
 splide.mount();
 
 const productObj = { ...productData.responses[0] };
-const userCategories = productObj[0].params.userCategories;
-const recommendedProducts = productObj[0].params.recommendedProducts;
+const userCategories = productObj[0].params.userCategories; // kategorilerin tutuldugu degisken
+const recommendedProducts = productObj[0].params.recommendedProducts; // json da yer alan tum urunleri kapsayan degerleri tutan degisken
+const categoriesList = document.querySelector(".categories");
 const productListContainer = document.querySelector(".product-list-container");
 const addToCartPopup = document.querySelector(".add-to-cart-popup");
 const closeIcon = document.querySelector(".close-icon");
-let selectedCategory = "Size Özel";
+
+// "Ev, Dekorasyon, Bahçe > Mobilya" seklinde gelen kategorileri duzenleyip "Mobilya" haline getirerek aktardigimiz dizi
 let editedUserCategories = userCategories.map((category) => {
   const categoryArr = category.split(">");
   return categoryArr[categoryArr.length - 1];
 });
-let categoriesList;
 let categoriesListItems;
 let addToCartButtons;
 
-console.log(recommendedProducts);
-
-showSelectedCategory(recommendedProducts[`Size Özel`]);
-
-categoriesList = document.querySelector(".categories");
-editedUserCategories.map((category, index) => {
-  const liEl = document.createElement("li");
-  liEl.classList.add("category-list");
-  liEl.innerHTML = `
-    <a class="category-list-item">${category}</a>
-  `;
-  index === 0 ? liEl.classList.add("active") : "";
-  categoriesList.append(liEl);
-});
-
-categoriesListItems = document.querySelectorAll(".category-list");
-categoriesListItems.forEach((item, index) => {
-  item.addEventListener("click", (event) => onCategory(event, index));
-});
-
+// kategoriler arasindan secilen kategoriye active classini eklemek ve secili kategoriye ait urunlerin listelenmesini saglamak icin gerekli fonksiyon cagrisini yapan fonksiyon
 function onCategory(e, i) {
   categoriesListItems.forEach((item) => {
     item.classList.remove("active");
   });
   e.target.classList.toggle("active");
-  selectedCategory = e.target.textContent.trim();
   showSelectedCategory(recommendedProducts[userCategories[i]]);
 }
 
+// secili kategoriye ait urunleri bir dizi halinde parametre olarak aldiktan sonra urunlerin yer alacagi kapsayici icine her urunun eklenmesini gerceklestiren fonksiyon
 function showSelectedCategory(categoryProducts) {
   productListContainer.innerHTML = ``;
   const { add } = splide.Components.Slides;
-  categoryProducts.forEach((category, index) => {
+  categoryProducts.forEach((category) => {
     const divEl = document.createElement("div");
-    divEl.classList.add("splide__slide");
     divEl.classList.add("product-container");
-    divEl.setAttribute("id", `splide01-slide0${index + 1}`);
     divEl.innerHTML = `
-      <img src="${category.image}" alt="aneirin" />
+      <img data-splide-lazy=${category.image} alt="Product Image" />
       <p class="product-title">${category.name}</p>
       <div class="price-container">
         <p class="price-text">${category.priceText}</p>
@@ -79,12 +61,15 @@ function showSelectedCategory(categoryProducts) {
     `;
     add(divEl);
   });
+
+  // product cardlarda yer alan Sepete Ekle butonlarina tiklanma aninda gerceklesecek fonksiyonun atamasinin yapilmasi
   addToCartButtons = document.querySelectorAll(".addToCart");
   addToCartButtons.forEach((button) => {
     button.addEventListener("click", onAddToCart);
   });
 }
 
+// Sepete Ekle butonuna tiklandiginda bilgilendirici popupin 2 saniyeligine gorunerek kaybolmasini saglayan fonksiyon
 function onAddToCart() {
   addToCartPopup.style.opacity = "1";
   setTimeout(() => {
@@ -92,7 +77,29 @@ function onAddToCart() {
   }, 2000);
 }
 
-closeIcon.addEventListener("click", onClose);
+// Popupta yer alan kapatma butona basildiginda popupin gorunurlugunu 0 yapan fonksiyon
 function onClose() {
   addToCartPopup.style.opacity = "0";
 }
+
+// sayfa acilisinda 'Size Ozel' kategorisine ait urunlerin getirilmesi
+showSelectedCategory(recommendedProducts[`Size Özel`]);
+
+closeIcon.addEventListener("click", onClose);
+
+// kategori menusunde yer alacak kategorilerin dinamik olarak DOM'a eklenmesi.
+editedUserCategories.map((category, index) => {
+  const liEl = document.createElement("li");
+  liEl.classList.add("category-list");
+  liEl.innerHTML = `
+    <a class="category-list-item">${category}</a>
+  `;
+  index === 0 ? liEl.classList.add("active") : "";
+  categoriesList.append(liEl);
+});
+
+// DOM'a eklenen kategorileri tutan li elementlerine click eventlerinin eklenmesi
+categoriesListItems = document.querySelectorAll(".category-list");
+categoriesListItems.forEach((item, index) => {
+  item.addEventListener("click", (event) => onCategory(event, index));
+});
